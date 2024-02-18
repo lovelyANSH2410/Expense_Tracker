ad0a0e9fc294dee89cd311a082d133d61aa70acb
Shubham Kushwaha <ansh.kush.2410@gmail.com>
2024-02-19 00:18:08 +0530

    Added delete expense

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -3,13 +3,18 @@ import ExpenseDate from "./ExpenseDate";
 import Card from "./Card";
 
 function ExpenseItem(props) {
+
+  const deleteExpense = () => {
+
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
           <h2>{props.title}</h2>
           <div className="expense-item__price">${props.amount}</div>
-          <div className="expense-item__location">{props.location}</div>
+          <button onClick={deleteExpense}>Delete expense</button>
       </div>
     </Card>
   );
